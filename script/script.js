68ad197706d578eca60e880fc458cc3dab995676
Igor <vov4enkoi32@gmail.com>
2020-11-18 15:35:31 +0300

    First comit

---
+++ script/script.js
@@ -0,0 +1,32 @@
+    "use strict";
+
+// a = 228;
+// console.log(a); 
+let number = 5;
+const letBorderWidht = 1;
+number = 10;
+console.log(number);
+// создание объектов
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+// console.log(obj.name); оброщение к объекту 
+// console.log(obj ["name"]); это тоже, но так лучше не оброщатся 
+
+//создание массивa
+// let arr = ['prikol.png', 'shreck.jpg', 'pornoSCobakami.mp4', 228]; //нумерация начинается с 0
+// // console.log(arr[1]); шрек
+// const answers = [];
+// answers[0] = prompt('Как ваше имя', '');
+// answers[1] = prompt("Ваша фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+
+// document.write(answers);
+let incr = 10,
+    decr = 10;
+incr++; //увеличение на 1
+decr--; //уменьшение на 1 
+console.log(incr);
+console.log(decr);
